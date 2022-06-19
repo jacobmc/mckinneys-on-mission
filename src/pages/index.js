1f9d934a2c4a43ba73f85288a0b6973cb05df4f2
Jacob McKinney <jmckinney@imageinabox.com>
2022-06-19 17:56:36 -0500

    Finalized form submission to mailchimp

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components"
 import ReCAPTCHA from "react-google-recaptcha"
+import addToMailchimp from "gatsby-plugin-mailchimp"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -16,16 +17,39 @@ const Container = styled.div`
   	margin: 0 auto;
   	padding: 50px 40px;
   
-  	section:last-child {
-	  padding-top: 50px;
-	}
+  	section:last-child { padding-top: 50px; }
+  	h2 { margin-top: 50px; }
   
-  	h2 {
-	  margin-top: 50px;
+  	@media (max-width: 900px) {
+	  grid-template-columns: 1fr;
+	  
+	  section:last-child { 
+		padding-top: 0; 
+		
+		h2 { margin-top: 0; }
+	  }
 	}
 `
 
-const Messages = styled.div``
+const Messages = styled.div`
+  		margin-bottom: 15px;
+  
+		p {
+		  margin-bottom: 0;
+		  padding: 5px 10px;
+		  border-radius: 5px;
+		}
+  
+  		.error {
+		  background: rgba(171, 35, 70, 0.8);
+		  color: #fff;
+		}
+
+	    .success {
+		  background: rgba(3, 121, 113, 0.7);
+		  color: #fff;
+	    }
+	`
 
 const Form = styled.form`
 	display: grid;
@@ -59,7 +83,8 @@ const Form = styled.form`
 	  }
 	  
 		&.email,
-		&.follow-up {
+		&.follow-up,
+		&.recaptcha {
 		  grid-column-start: span 2;
 		}
 	}
@@ -83,26 +108,45 @@ const IndexPage = () => {
 		const recaptchaValue = recaptchaRef.current.getValue(),
 			msgContainer = document.getElementById('messages'),
 			errors = [],
-			messages = []
+			messages = [],
+			listFields = {
+				FNAME: firstName,
+				LNAME: lastName,
+				FOLLOW_UP: ( followUp ) ? 'Yes' : 'No'
+			}
 
 		if (recaptchaValue === '') {
 			errors.push('Please complete the ReCAPTCHA check.');
 		}
 
-		msgContainer.innerHTML = '';
+		addToMailchimp(email, listFields)
+			.then(data => {
+				if ( data.result === 'error' ) {
+					errors.push(data.msg)
+				} else {
+					messages.push(data.msg)
+				}
 
-		if (errors.length) {
-			errors.forEach(error => {
-				let errorP = document.createElement('p')
-				errorP.classList.add('error')
-				errorP.appendChild(document.createTextNode(error))
-				msgContainer.appendChild(errorP)
+				msgContainer.innerHTML = '';
+
+				if ( messages.length ) {
+					messages.forEach( msg => {
+						let msgP = document.createElement('p')
+						msgP.classList.add('success')
+						msgP.appendChild(document.createTextNode(msg))
+						msgContainer.appendChild(msgP)
+					})
+				}
+
+				if (errors.length) {
+					errors.forEach(error => {
+						let errorP = document.createElement('p')
+						errorP.classList.add('error')
+						errorP.appendChild(document.createTextNode(error))
+						msgContainer.appendChild(errorP)
+					})
+				}
 			})
-
-			return;
-		}
-
-		// TODO add mailchimp functionality
 	}
 
 	return (
@@ -112,7 +156,7 @@ const IndexPage = () => {
 					<StaticImage src={"../images/mckinney-family.jpeg"} title={"The McKinneys"} alt={"A photo of the McKinney family sitting on a bench."} />
 					<h2>Hey There!</h2>
 					<p>We are Jacob & Shawnda McKinney and we (and our three kids) are heading to Scotland to join in the work God is doing there. We will be working with other MTW (Mission to the World) missionaries to help support the Free Church of Scotland in their vision to plant 30 churches by 2030.</p>
-					<p>If you'd like to keep up with us and what God is doing in our lives and ministry, fill out the form below to sign up for our newsletter. If you'd like to meet with us to learn more about our vision and discuss potentially partnering with us in the work we are doing, check the box at the bottom of the form and we will contact you to set something up.</p>
+					<p>If you'd like to keep up with us and what God is doing in our lives and ministry, fill out the form to sign up for our newsletter. If you'd like to meet with us to learn more about our vision and discuss potentially partnering with us in the work we are doing, check the box at the bottom of the form and we will contact you to set something up.</p>
 				</section>
 				<section>
 					<h2>Join Our Newsletter</h2>
@@ -120,22 +164,22 @@ const IndexPage = () => {
 					<Form id={"newsletter-form"	} method={'post'} onSubmit={submitForm}>
 						<div className={'field first-name'}>
 							<label htmlFor={'first-name'}>First Name:</label>
-							<input name={'first-name'} type={'text'} value={firstName} onChange={e => setFirstName(e.target.value)} />
+							<input id={'first-name'} name={'first-name'} type={'text'} value={firstName} onChange={e => setFirstName(e.target.value)} />
 						</div>
 						<div className={'field last-name'}>
 							<label htmlFor={'last-name'}>Last Name:</label>
-							<input name={'last-name'} type={'text'} value={lastName} onChange={e => setLastName(e.target.value)} />
+							<input id={'last-name'} name={'last-name'} type={'text'} value={lastName} onChange={e => setLastName(e.target.value)} />
 						</div>
 						<div className={'field email'}>
-							<label htmlFor={'email'}>Email:</label>
-							<input name={'email'} type={'email'} value={email} onChange={e => setEmail(e.target.value)} required />
+							<label htmlFor={'email'}>Email (required):</label>
+							<input id={'email'} name={'email'} type={'email'} value={email} onChange={e => setEmail(e.target.value)} required />
 						</div>
 						<div className={'field follow-up'}>
-							<input name={'follow-up'} type={'checkbox'} value={followUp} onChange={e => setFollowUp(e.target.value)} />
+							<input id={'follow-up'} name={'follow-up'} type={'checkbox'} value={followUp} onClick={e => setFollowUp(e.target.checked)} />
 							<label htmlFor={'follow-up'}>I would like to learn more about your vision and a potential partnership.</label>
 						</div>
 
-						<ReCAPTCHA ref={recaptchaRef} sitekey={process.env.GATSBY_RECAPTCHA_SITE_KEY} />
+						<ReCAPTCHA className={'field recaptcha'} ref={recaptchaRef} sitekey={process.env.GATSBY_RECAPTCHA_SITE_KEY} />
 
 						<button type={'submit'}>Submit</button>
 					</Form>
